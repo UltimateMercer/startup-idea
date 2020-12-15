014c852ca729895f5bcb5c1b7a762f54ee9be700
Julian Silva da Cunha <juliancunha2010@hotmail.com>
2020-12-15 17:08:11 -0300

    Profile

---
+++ pages/profile.vue
@@ -0,0 +1,162 @@
+<template>
+  <div class="py-3">
+    <div class="container-fluid">
+      <div class="row">
+        <section class="col-lg-5 col-md-6 col-12">
+          <div class="px-2 py-3 sticky-top">
+            <div class="card card-plain card-body mx-md-4">
+              <img
+                src="https://i.imgur.com/VuENQgt.jpg"
+                class="rounded-lg shadow-image mx-auto d-block my-3"
+                alt=""
+                style="width: 300px; height: 300px; object-fit: cover"
+              />
+              <h2 class="text-center">
+                <strong><em>Ultimate</em></strong>
+              </h2>
+              <div class="text-center mb-3">
+                <h3>Informações de Contato</h3>
+                <div>
+                  <button class="btn btn-dark">
+                    <font-awesome-icon :icon="['fab', 'facebook-f']" />
+                  </button>
+                  <button class="btn btn-dark">
+                    <font-awesome-icon :icon="['fab', 'twitter']" />
+                  </button>
+                  <button class="btn btn-dark">
+                    <font-awesome-icon :icon="['fab', 'linkedin']" />
+                  </button>
+                  <button class="btn btn-dark">
+                    <font-awesome-icon :icon="['fab', 'instagram']" />
+                  </button>
+                  <button class="btn btn-dark">
+                    <font-awesome-icon :icon="['fas', 'envelope']" />
+                  </button>
+                </div>
+              </div>
+            </div>
+            <!-- <div>
+              <img
+                src="https://i.imgur.com/DtVy4kL.jpg"
+                class="rounded-lg shadow-image mx-auto d-block my-3"
+                alt=""
+                style="width: 350px; height: 350px; object-fit: cover"
+              />
+              <h2 class="text-center">
+                <strong><em>Ultimate</em></strong>
+              </h2>
+              <div class="text-center mb-3">
+                <h3>Informações de Contato</h3>
+                <div>
+                  <button class="btn btn-dark">1</button>
+                  <button class="btn btn-dark">2</button>
+                  <button class="btn btn-dark">3</button>
+                  <button class="btn btn-dark">4</button>
+                  <button class="btn btn-dark">5</button>
+                </div>
+              </div>
+            </div> -->
+          </div>
+        </section>
+        <section class="col-lg-7 col-md-6 col-12">
+          <div class="px-3 py-2 mt-4">
+            <h2 class="mb-4"><strong> Sobre Mim </strong></h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+              aspernatur ullam sequi eos consectetur blanditiis a minima
+              necessitatibus nihil! Reprehenderit nesciunt numquam sit labore
+              amet dolor perspiciatis facilis est in.
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat
+              explicabo nemo quae blanditiis a itaque corporis mollitia
+              cupiditate, reiciendis, accusantium nobis animi consectetur.
+              Aspernatur doloremque consequuntur, recusandae dolor rerum quas.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+              mollitia odio amet magni blanditiis laudantium saepe id unde
+              distinctio, praesentium nostrum, corporis at voluptatem, est iusto
+              optio expedita veritatis sint!
+            </p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum
+              nulla natus, soluta quia impedit consectetur provident ut deleniti
+              reiciendis, esse obcaecati possimus fuga ea dolorem, et ducimus
+              placeat praesentium dolores!
+            </p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error
+              hic maiores fugit optio? Molestiae, atque, quidem laborum
+              consequuntur dolorem rem ea, reprehenderit libero non ullam
+              excepturi impedit dolor architecto consectetur.
+            </p>
+          </div>
+          <div class="px-3 py-4">
+            <h2 class="mb-4">Conhecimento técnico</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure
+              quaerat officia distinctio neque velit officiis nostrum deserunt
+              cum nesciunt veritatis et laudantium, cumque suscipit, natus
+              voluptatibus repellat odit tempore ipsam.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+              ratione cupiditate exercitationem quasi labore molestiae molestias
+              ex ad ipsam beatae omnis, earum in? Modi quis aut, eum beatae
+              maiores id?
+            </p>
+            <div>
+              <span class="badge badge-primary">Design</span>
+              <span class="badge badge-primary">Programação</span>
+              <span class="badge badge-primary"> UX/UI </span>
+              <span class="badge badge-primary"> Front-end </span>
+              <span class="badge badge-primary">Ilustração</span>
+              <span class="badge badge-primary">Back-end</span>
+            </div>
+          </div>
+          <div class="px-3 py-4">
+            <h2 class="mb-4">Projetos</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure
+              quaerat officia distinctio neque velit officiis nostrum deserunt
+              cum nesciunt veritatis et laudantium, cumque suscipit, natus
+              voluptatibus repellat odit tempore ipsam.
+            </p>
+
+            <div class="card">
+              <img
+                src="https://i.imgur.com/86pDNNi.png?1"
+                style="width: 100%; height: 125px; object-fit: cover"
+                alt=""
+              />
+              <div class="card-img-overlay d-flex">
+                <div class="my-auto text-light"><h4>Projeto: blklist</h4></div>
+              </div>
+            </div>
+            <div class="card">
+              <img
+                src="https://i.imgur.com/86pDNNi.png?1"
+                style="width: 100%; height: 125px; object-fit: cover"
+                alt=""
+              />
+              <div class="card-img-overlay d-flex">
+                <div class="my-auto text-light"><h4>Projeto: blklist</h4></div>
+              </div>
+            </div>
+            <div class="card">
+              <img
+                src="https://i.imgur.com/86pDNNi.png?1"
+                style="width: 100%; height: 125px; object-fit: cover"
+                alt=""
+              />
+              <div class="card-img-overlay d-flex">
+                <div class="my-auto text-light"><h4>Projeto: blklist</h4></div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
